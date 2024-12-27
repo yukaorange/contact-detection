a9cf7680e2ddad9ea2ed12cf3033e8f170754309
yukaorange <takaoka.webdesigner@gmail.com>
2024-12-28 08:18:22 +0900

    接触判定と疑似球体エリアの衝突判定を融合

--- src/shaders/sphere/fragment.glsl
+++ src/shaders/sphere/fragment.glsl
@@ -13,6 +13,7 @@ varying vec2 vUv;
 varying vec3 vWorldPosition;
 
 #include ../utils/readDepth.glsl
+#include ../utils/blend.glsl
 
 void main() {
 
@@ -28,20 +29,20 @@ void main() {
   float contactAmount = depth - normalizedDistance;
 
   //接触量を最大値にはじく閾値
-  float contactDetectEdge = 0.02;
+  float contactDetectEdge = 0.01;
 
   float contactDiffuseValue = smoothstep(0.0, contactDetectEdge, contactAmount);
 
   //球体の接触判定
-  float distanceFromContact = length(vWorldPosition - contactPoint);
+  float distanceFromContact = clamp(length(vWorldPosition - contactPoint), 0.0, 1.0);//clampしておかないと、影響範囲が制限できない。
 
   //------- デバッグ -------
   vec3 depthColor = vec3(1.0 - depth);
   vec3 distanceColor = vec3(1.0 - normalizedDistance);
   vec3 contactColor = vec3(1.0 - contactDiffuseValue);
-  vec3 distanceFromContactColor = vec3(1.0 - distanceFromContact);
+  vec3 distanceFromContactColor = vec3(1.0 - distanceFromContact) * uContactIntensity;
 
-  vec3 test = distanceFromContactColor * uContactIntensity;
+  vec3 test = contactColor + distanceFromContactColor;
   // test = vec3(fragCoord, 0.0);
 
   //------- 出力 -------
